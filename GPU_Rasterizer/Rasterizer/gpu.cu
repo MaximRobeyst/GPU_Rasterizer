e0e864091b213bd8a4c15f0549634c4277a9cb78
TheThrashMan81 <maxim.robeyst@gmail.com>
2022-11-26 19:51:42 +0100

    base framework

---
+++ GPU_Rasterizer/Rasterizer/gpu.cu
@@ -0,0 +1,46 @@
+#include "gpu.h"
+#include <driver_types.h>
+#include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+
+uint32_t* gpuAlloc(void) {
+	uint32_t* gpu_mem;
+
+	cudaError_t err = cudaMalloc(&gpu_mem, SCREEN_SIZE * 4);
+	if (err != cudaSuccess) return NULL;
+
+	return gpu_mem;
+};
+
+void gpuFree(void* gpu_mem) {
+	cudaFree(gpu_mem);
+}
+
+int gpuBlit(void* src, void* dst) {
+	cudaError_t err = cudaMemcpy(dst, src, SCREEN_SIZE * 4, cudaMemcpyDeviceToHost);
+	if (err != cudaSuccess) return 1;
+	return 0;
+}
+
+// ----- 
+
+__host__
+__device__
+uint32_t getPixColor(int x, int y) {
+	return 0xFFFF0000;
+}
+
+__global__ void my_kernel(uint32_t* buf) {
+	const int xPix = blockDim.x * blockIdx.x + threadIdx.x;
+	const int yPix = blockDim.y * blockIdx.y + threadIdx.y;
+
+	unsigned int pos = SCREEN_WIDTH * yPix + xPix;
+
+	buf[pos] = getPixColor(xPix, yPix);
+}
+
+void gpuRender(uint32_t* buf) {
+	const dim3 blocksPerGrid(H_TILES, V_TILES);
+	const dim3 threadsPerBlock(TILE_WIDTH, TILE_HEIGHT);
+	my_kernel <<<blocksPerGrid, threadsPerBlock>>> (buf);
+}
---
+++ GPU_Rasterizer/Rasterizer/gpu.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "const.h"
+
+uint32_t* gpuAlloc(void);
+void gpuFree(void* gpu_mem);
+int gpuBlit(void* src, void* dst);
+
+void gpuRender(uint32_t* buf);
