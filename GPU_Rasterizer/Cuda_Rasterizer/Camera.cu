76496fcb67bd2bb91adf68786c1eb4100a89ea37
TheTwitchDude81 <51368164+MaximRobeyst@users.noreply.github.com>
2022-12-14 12:40:06 +0100

    Add camera

--- GPU_Rasterizer/Cuda_Rasterizer/Camera.cu
+++ GPU_Rasterizer/Cuda_Rasterizer/Camera.cu
@@ -1,6 +1,13 @@
 #include "Camera.h"
+#include "const.h"
 
 Camera::Camera(const glm::vec3& position, const glm::vec3& forward, float FOV, float aspectRatio, float far, float near)
+	: m_Position{position}
+	, m_Forward{forward}
+	, m_FOV{FOV * static_cast<float>(E_TO_RADIANS)}
+	, m_AspectRatio{aspectRatio}
+	, m_Far{far}
+	, m_Near{near}
 {
 
 	m_ProjectionMatrix = glm::mat4{
@@ -13,4 +20,67 @@ Camera::Camera(const glm::vec3& position, const glm::vec3& forward, float FOV, f
 
 void Camera::UpdateMatrix()
 {
+	//FORWARD (zAxis) with YAW applied
+	glm::mat3 yawRotation = MakeRotationY(m_AbsoluteRotation.y * float(E_TO_RADIANS));
+	glm::vec3 zAxis = yawRotation * m_Forward;
+
+	//Calculate RIGHT (xAxis) based on transformed FORWARD
+	glm::vec3 xAxis = glm::normalize(glm::cross(glm::vec3{ 0.f,1.f,0.f }, zAxis));
+
+	//FORWARD with PITCH applied (based on xAxis)
+	glm::mat3 pitchRotation = MakeRotation(m_AbsoluteRotation.x * float(E_TO_RADIANS), xAxis);
+	zAxis = pitchRotation * zAxis;
+
+	//Calculate UP (yAxis)
+	glm::vec3 yAxis = glm::cross(zAxis, xAxis);
+
+	//Translate based on transformed axis
+	m_Position += m_RelativeTranslation.x * xAxis;
+	m_Position += m_RelativeTranslation.y * yAxis;
+	m_Position += m_RelativeTranslation.z * zAxis;
+
+	//Construct View2World Matrix
+	m_ViewToWorld =
+	{
+		glm::vec4{xAxis, 0.0f},
+		glm::vec4{yAxis, 0.0f},
+		glm::vec4{zAxis, 0.0f},
+		glm::vec4{m_Position.x,m_Position.y,m_Position.z,1.f}
+	};
+
+	//Construct World2View Matrix || viewMatrix
+	m_WorldToView = glm::inverse(m_ViewToWorld);
+
+	// WorldViewProjectionMatrix = ProjectionMatrix * ViewMatrix * WorldMatrix
+	m_WorldViewProjectionMatrix = m_ProjectionMatrix * m_ViewToWorld;
+}
+
+glm::mat3 Camera::MakeRotationY(float t)
+{
+	float c = cosf(t);
+	float s = sinf(t);
+
+	return glm::mat3(
+		c, 0, s,
+		0,1,0,
+		-s, 0, c);
+}
+
+glm::mat3 Camera::MakeRotation(float t, glm::vec3 axis)
+{
+	float c = cosf(t);
+	float s = sinf(t);
+	float oneMinusC = (1 - c);
+
+	float xy = axis.x * axis.y;
+	float yz = axis.y * axis.z;
+	float xz = axis.x * axis.z;
+	float sx = s * axis.x;
+	float sy = s * axis.y;
+	float sz = s * axis.z;
+
+	return glm::mat3(
+		c + oneMinusC * (axis.x * axis.x), oneMinusC * xy - sz, oneMinusC * xz + sy,
+		oneMinusC * xy + sz, c + oneMinusC * (axis.y * axis.y), oneMinusC * yz - sx,
+		oneMinusC * xz - sy, oneMinusC * yz + sx, c + oneMinusC * (axis.z * axis.z));
 }
--- GPU_Rasterizer/Cuda_Rasterizer/Camera.h
+++ GPU_Rasterizer/Cuda_Rasterizer/Camera.h
@@ -8,6 +8,20 @@ public:
 
 private:
 	void UpdateMatrix();
+	glm::mat3 MakeRotationY(float f);
+	glm::mat3 MakeRotation(float f, glm::vec3 axis);
+
+	const float m_AspectRatio;
+	const float m_Far;
+	const float m_Near;
+
+	float m_FOV;
+
+	glm::vec3 m_Position;
+	glm::vec3 m_Forward{};
+
+	glm::vec2 m_AbsoluteRotation{}; //Pitch(x) & Yaw(y) only
+	glm::vec3 m_RelativeTranslation{};
 
 	glm::mat4 m_WorldToView{};
 	glm::mat4 m_ViewToWorld{};
